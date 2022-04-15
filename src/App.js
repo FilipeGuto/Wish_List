496dda6c0ae3dbd31b6c31080b7b474940f8ef35
FilipeGuto <filipeguto95@gmail.com>
2022-04-15 15:52:16 -0300

    FEAT: Header e informações importantes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Components/Header/Header";
+import Info from "./Components/Info/Info";
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+        <Info />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Alert } from "reactstrap";
+import "./header.css"
+
+export default function Header() {
+  return (
+    <div>
+  <Alert
+    className="header"
+  >
+    Wishlist 💝
+  </Alert>
+</div>
+  );
+}
